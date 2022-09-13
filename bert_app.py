de581e0f9159eda2740bd657e5777ac47eac9219
nicolorosso <98038058+nicolorosso@users.noreply.github.com>
2022-09-13 22:58:51 +0200

    Update bert_app.py

--- bert_app.py
+++ bert_app.py
@@ -6,7 +6,7 @@ auth_key = "9d5d6377-86f6-5862-90e2-783b691526a2:fx"
 translator = deepl.Translator(auth_key)
 
 from summarizer import Summarizer
-from transformers import pipeline
+#from transformers import pipeline
 
 
 
@@ -22,23 +22,23 @@ def translate(text):
 
 
 
-@st.cache
-def translate3(text):
-  new_text = translator.translate_text(text, target_lang="EN-GB")
-  body = new_text.text
-  model = pipeline("summarization")
-  result = model(body, min_length=25, max_length=60)
-  final_text = translator.translate_text(result, target_lang="IT")
-  return final_text.text
+#@st.cache
+#def translate3(text):
+  #new_text = translator.translate_text(text, target_lang="EN-GB")
+  #body = new_text.text
+  #model = pipeline("summarization")
+  #result = model(body, min_length=25, max_length=60)
+  #final_text = translator.translate_text(result, target_lang="IT")
+  #return final_text.text
 
-@st.cache
-def translate4(text):
-  new_text = translator.translate_text(text, target_lang="EN-GB")
-  body = new_text.text
-  model = pipeline("summarization", model="t5-large", tokenizer="t5-large", framework="tf")
-  result = model(body, min_length=25, max_length=60)
-  final_text = translator.translate_text(result, target_lang="IT")
-  return final_text.text
+#@st.cache
+#def translate4(text):
+  #new_text = translator.translate_text(text, target_lang="EN-GB")
+  #body = new_text.text
+  #model = pipeline("summarization", model="t5-large", tokenizer="t5-large", framework="tf")
+  #result = model(body, min_length=25, max_length=60)
+  #final_text = translator.translate_text(result, target_lang="IT")
+  #return final_text.text
 
 
 
@@ -48,34 +48,34 @@ def main():
 	
 
 	# Title
-	st.title("News Summarizer")
+	st.title("Demo di News Summarizer")
 	st.subheader("Riassumere testi in pochi semplici click")
 	
 
 	# Summarization
 	if st.checkbox("Get the summary of your text"):
-		st.subheader("Summarize Your Text")
+		st.subheader("Sto lavorando anche con altri modelli, per il momento condivido questa demo con BERT ")
 
 		message = st.text_area("Enter Text","Type Here....")
-		summary_options = st.selectbox("Choose Summarizer",['bert','pegasus', 'pipeline api', 't-5 large'])
+		summary_options = st.selectbox("Choose Summarizer",['bert','cerchero in settimana di caricare anche altri modelli per avere più confronti su performance'])
 		if st.button("Summarize"):
 			if summary_options == 'bert':
 				st.text("Using Bert Summarizer ..")
 				summary_result = translate(message)
 			
-			elif summary_options == 'pipeline api':
-				st.text("Using Pipeline API Summarizer ..")
-				summary_result = translate3(message)
+			#elif summary_options == 'pipeline api':
+				#st.text("Using Pipeline API Summarizer ..")
+				#summary_result = translate3(message)
 				
-			elif summary_options == 't-5 large':
-				st.text("Using t-5 Summarizer ..")
-				summary_result = translate4(message)
+			#elif summary_options == 't-5 large':
+				#st.text("Using t-5 Summarizer ..")
+				#summary_result = translate4(message)
 				
-			else:
-				st.warning("Using Default Summarizer")
-				st.text("Using Gensim Summarizer ..")
-				summary_result = translate(message)
-			st.success(summary_result)
+			#else:
+				#st.warning("Using Default Summarizer")
+				#st.text("Using Gensim Summarizer ..")
+				#summary_result = translate(message)
+			#st.success(summary_result)
 
 if __name__ == '__main__':
 	main()
