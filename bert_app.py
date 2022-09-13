09249d91097452e2455d0b836ca0dfd9ddd52d5e
nicolorosso <98038058+nicolorosso@users.noreply.github.com>
2022-09-13 18:48:29 +0200

    Update bert_app.py

--- bert_app.py
+++ bert_app.py
@@ -5,8 +5,8 @@ import deepl
 auth_key = "9d5d6377-86f6-5862-90e2-783b691526a2:fx" 
 translator = deepl.Translator(auth_key)
 
-from summarizer import Summarizer,TransformerSummarizer
-from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
+from summarizer import Summarizer
+from transformers import pipeline
 
 tokenizer = AutoTokenizer.from_pretrained("google/pegasus-multi_news")
 
@@ -21,14 +21,7 @@ def translate(text):
   final_text = translator.translate_text(result, target_lang="IT")
   return final_text.text
 
-@st.cache
-def translate2(text):
-  new_text = translator.translate_text(text, target_lang="EN-GB")
-  body = new_text.text
-  model = AutoModelForSeq2SeqLM.from_pretrained("google/pegasus-multi_news")
-  result = model(body, min_lenghts=80)
-  final_text = translator.translate_text(result, target_lang="IT")
-  return final_text.text
+
 
 
 @st.cache
@@ -71,15 +64,15 @@ def main():
 			if summary_options == 'bert':
 				st.text("Using Bert Summarizer ..")
 				summary_result = translate(message)
-			elif summary_options == 'pegasus':
-				st.text("Using Pegasus Summarizer ..")
-				summary_result = translate2(message)
+			
 			elif summary_options == 'pipeline api':
 				st.text("Using Pipeline API Summarizer ..")
 				summary_result = translate3(message)
+				
 			elif summary_options == 't-5 large':
 				st.text("Using t-5 Summarizer ..")
 				summary_result = translate4(message)
+				
 			else:
 				st.warning("Using Default Summarizer")
 				st.text("Using Gensim Summarizer ..")
