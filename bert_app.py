e443d57c9d5b3f5d948be0a85a1f6690372406db
nicolorosso <98038058+nicolorosso@users.noreply.github.com>
2022-09-13 17:03:27 +0200

    Create bert_app.py

---
+++ bert_app.py
@@ -0,0 +1,45 @@
+import deepl
+auth_key = "9d5d6377-86f6-5862-90e2-783b691526a2:fx" 
+translator = deepl.Translator(auth_key)
+
+from summarizer import Summarizer
+
+def translate(text):
+  new_text = translator.translate_text(text, target_lang="EN-GB")
+  body = new_text.text
+  model = Summarizer('distilbert-base-uncased', hidden=[-1,-2], hidden_concat=True)
+  result = model(body, num_sentences=3)
+  final_text = translator.translate_text(result, target_lang="IT")
+  return final_text.text
+  
+
+def main():
+	""" NLP Based App with Streamlit """
+
+	# Title
+	st.title("Ultimate NLP Application")
+	st.subheader("Natural Language Processing for everyone")
+	st.markdown("""
+    	#### Description
+    	+ This is a Natural Language Processing(NLP) Based App useful for basic NLP task
+    	Tokenization , Lemmatization, Named Entity Recognition (NER), Sentiment Analysis, Text Summarization. Built for social good by [LekanAkin](https://github.com/lekanakin). Click any of the checkboxes to get started.
+    	""")
+
+	# Summarization
+	if st.checkbox("Get the summary of your text"):
+		st.subheader("Summarize Your Text")
+
+		message = st.text_area("Enter Text","Type Here....")
+		summary_options = st.selectbox("Choose Summarizer",['sumy','gensim'])
+		if st.button("Summarize"):
+			if summary_options == 'bert':
+				st.text("Using Sumy Summarizer ..")
+				summary_result = translate(message)
+			elif summary_options == 'gensim':
+				st.text("Using Gensim Summarizer ..")
+				summary_result = summarize(message)
+			else:
+				st.warning("Using Default Summarizer")
+				st.text("Using Gensim Summarizer ..")
+				summary_result = summarize(message)
+			st.success(summary_result)
